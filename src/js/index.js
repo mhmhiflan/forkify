c616edcc3d7c74d548c085386757d30c86015c01
Mohamed Hiflan <HiflanH@mitrai.local>
2020-01-02 16:39:59 +0530

    searchview results loaded

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,26 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
-const search = new Search('pizza');
+const state = {};
 
-search.getResults();
-
-const constrolSearch =  () => {
+const controlSearch = async () => {
     //1.Get query from view
-
+    const query = searchView.getSearchInput();
+    console.log(query);
     //2.new state object and add to state
-
+    state.search = new Search(query);
     //3.prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearSearchList();
     //4.get search results
-
+    await state.search.getResults();
     //5.Display results on UI
+    searchView.renderRecipies(state.search.result);
 
 }
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
     e.preventDefault();
     controlSearch();
 })
